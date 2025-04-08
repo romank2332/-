1dcdb1b0e8b385318cbca8dc16af0b2bf6d8616c
roma_pb1 <roman.k@freezza.pro>
2025-04-08 12:16:11 +0300

    исправлено

--- exercises/07_files/task_7_3b.py
+++ exercises/07_files/task_7_3b.py
@@ -17,3 +17,11 @@ Enter VLAN number: 10
 Ограничение: Все задания надо выполнять используя только пройденные темы.
 
 """
+polzovatel_vlan = input("Enter VLAN number: ")
+
+with open("CAM_table.txt", "r") as conf:
+    for line in conf:
+        slova = line.split()
+        if slova and slova[0].isdigit() and slova[0] == polzovatel_vlan:
+            vlan, mac, _, intf = slova
+            print(f"{vlan:9}{mac:20}{intf}")
